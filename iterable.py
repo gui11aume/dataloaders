38ab36cf4055d1d37619087cc92e871d3e9ee53f
gui11aume <guillaume.filion@gmail.com>
2023-04-22 23:25:07 -0400

    Add type hints and VScode format

--- iterable.py
+++ iterable.py
@@ -4,21 +4,24 @@ import gzip
 import os
 import torch
 
+from typing import Any, Iterator
+from torch.utils.data._utils.worker import WorkerInfo
 
-class IterableData(torch.utils.data.IterableDataset):
+
+class IterableData(torch.utils.data.IterableDataset):  # type: ignore
     """
     Defines the logic for iterable datasets (working over streams of
     data) in parallel multi-processing environments, e.g., multi-GPU.
     """
 
     @property
-    def iterator(self):
+    def iterator(self) -> Iterator[Any]:
         # Extend this class to define the stream.
         raise NotImplementedError
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[Any]:
         # Get worker info if in multi-processing context.
-        worker_info = torch.utils.data.get_worker_info()
+        worker_info: WorkerInfo = torch.utils.data.get_worker_info()  # type: ignore
         if worker_info is None:
             return self.iterator
 
@@ -27,61 +30,59 @@ class IterableData(torch.utils.data.IterableDataset):
         # the items of the stream to the workers.
         process_rk = int(os.environ.get("LOCAL_RANK", 0))
         process_nb = int(os.environ.get("WORLD_SIZE", 1))
-        local_worker_rk = worker_info.id
-        local_worker_nb = worker_info.num_workers
+        local_worker_rk: int = int(worker_info.id)  # type: ignore
+        local_worker_nb: int = int(worker_info.num_workers)  # type: ignore
         # Assume that each process has the same number of local workers.
-        worker_rk = process_rk * local_worker_nb + local_worker_rk
-        worker_nb = process_nb * local_worker_nb
+        worker_rk: int = process_rk * local_worker_nb + local_worker_rk
+        worker_nb: int = process_nb * local_worker_nb
 
         return itertools.islice(self.iterator, worker_rk, None, worker_nb)
 
 
-class DataFromIterator(IterableData):
+class IterableJSONData(IterableData):
+    "Iterate over the lines of a JSON file and uncompress if needed."
 
-    def __init__(self, iterator, train=True)
-        torch.utils.data.IterableDataset.__init__(self)
-        self._iterator = iterator
+    def __init__(self, data_path: str, train: bool = True):
+        super().__init__()  # type: ignore
+        self.data_path = data_path
         self.train = train
 
     @property
     def iterator(self):
-        return self._iterator
-
-
-class IterableDataFromFile(IterableData):
-
-    def __init__(self, path, train=True, encoding="ascii"):
-        torch.utils.data.IterableDataset.__init__(self)
-        self.path = path
-        self.train = train
-        self.encoding = encoding
-
-    def load(self, x):
-        raise NotImplementedError
-
-    @property
-    def iterator(self):
+        "Define line-by-line iterator for json file."
         # Read the magic number.
         with open(self.data_path, "rb") as f:
             magic_number = f.read(2)
         # If file is gzipped, uncompress it on the fly.
-        if magic_number == b'\x1f\x8b':
-            iterator = map(
-                    lambda line: self.load(line.decode(self.encoding)),
-                    gzip.open(self.path)
+        if magic_number == b"\x1f\x8b":
+            file_iterator = map(
+                lambda line: json.loads(line.decode("ascii")), gzip.open(self.data_path)
             )
         else:
+            file_iterator = map(lambda line: json.loads(line), open(self.data_path))
+        return file_iterator
+
+
+class IterableTextData(IterableData):
+    "Iterate over the lines of a text file and uncompress if needed."
+
+    def __init__(self, data_path: str, train: bool = True, encoding: str = "ascii"):
+        super().__init__()  # type: ignore
+        self.data_path = data_path
+        self.train = train
+        self.encoding = encoding
+
+    @property
+    def iterator(self):
+        "Define line-by-line iterator for text file."
+        # Read the magic number.
+        with open(self.data_path, "rb") as f:
+            magic_number = f.read(2)
+        # If file is gzipped, uncompress it on the fly.
+        if magic_number == b"\x1f\x8b":
             iterator = map(
-                    lambda line: self.load(line),
-                    open(self.path)
+                lambda line: line.decode(self.encoding), gzip.open(self.data_path)
             )
+        else:
+            iterator = map(lambda line: line, open(self.data_path))
         return iterator
-
-
-class IterableTextData(IterableDataFromFile):
-    def load(self, x):
-        return x
-
-class IterableJSONData(IterableDataFromFile):
-    def load(self, x):
-        return json.loads(x)
